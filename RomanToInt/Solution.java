6d6787b7013d3c38e8fe543af0c741ebbd2476be
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2025-10-02 19:48:13 +0000

    RomanToInt problem

---
+++ RomanToInt/Solution.java
@@ -0,0 +1,37 @@
+package RomanToInt;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    private static final Map<Character, Integer> romanValues = new HashMap<>();
+
+    static{
+        romanValues.put('I',1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L',50);
+        romanValues.put('C',100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+    }
+
+public int romanToInt(String s){
+    int ValorFinal = 0;
+    for (int i = 0; i < s.length(); i++) {
+        int ValorAtual = romanValues.get(s.charAt(i));
+        if (i+1 < s.length()) {
+            int valorProximo = romanValues.get(s.charAt(i+1));
+            if (ValorAtual < valorProximo) {
+                ValorFinal += valorProximo - ValorAtual;
+                i++;
+                continue;
+            }
+            ValorFinal += ValorAtual;
+        } else {
+            ValorFinal += ValorAtual;
+        }
+    }
+    return ValorFinal;
+}
+}
